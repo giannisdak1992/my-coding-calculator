287f233322aa14c52c5f56e9f058a23fedcefae6
Yiannis Ntakoumis <giannisdak@gmail.com>
2025-01-07 01:07:05 +0200

    hotfix. try catch added for handling divide by zero exception

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -27,6 +27,11 @@ public class Main {
         return a * b;
     }
     public static int mod (int a, int b) {
-        return a % b;
+        try {
+            return a % b;
+        }catch (ArithmeticException e) {
+            System.err.println("Error. Denominator must not be zero");
+            throw e;
+        }
     }
 }
